68ecee364d15562a074aa864ee738e3acceaf2b0
Elmira-86 <120679545+Elmira-86@users.noreply.github.com>
2022-12-20 21:47:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿//Домашнее задание 1
+
+// int SecondNumber(int num)
+// {
+//     int sot = num/10;
+//     int ed = sot%10;
+//     return ed;
+// }
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(SecondNumber(num));
+
+//Домашнее задание 2
+
+// int ThirdNumber (int num)
+// {
+// if(num >= 100 && num < 1000)
+// {
+//     num = num % 10;
+//     return num;
+// }
+// else
+// {
+//     if (num >=1000)
+// {
+// while(num >=1000)
+// {
+//     num =num / 10;
+// }
+// num = num % 10;
+// return num;
+// }
+// else
+// {
+//     Console.WriteLine($"{num} - Третьей цифры нет");
+//     return num;
+// }
+// }
+// }
+
+// Console.WriteLine ("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine ());
+// Console.WriteLine(ThirdNumber(num));
+
+//Домашнее задание 3
+
+// int Week (int num)
+// {
+//     if(num > 5)
+//     {
+//         Console.WriteLine($"{num} - да");
+//         return num;
+//         }
+//         else
+//         { 
+//             Console.WriteLine($"{num} - нет");
+//             return num;
+//             }
+// }
+
+// Console.WriteLine ("Введите день недели");
+// int num = Convert.ToInt32(Console.ReadLine ());
+// Console.WriteLine(Week(num));
+
